735275859d7efaa8419e05fb4a3f2eb7834afb0e
Carlos Hernandez <carloshdmn@gmail.com>
2018-03-05 00:18:13 +0100

    Add a new test to model validator
    Still untested

--- spec/validators/model.spec.js
+++ spec/validators/model.spec.js
@@ -9,16 +9,25 @@
  *	Lot of things to think about.
  */
 
-const valid = require('../../lib/validators/model');
+// const valid = require('../../lib/validators/model');
+
+const validate = {
+	valid: require('../../lib/validators/model')
+};
+const valid = validate.valid;
 
 describe(' - Model validator', () => {
 	it('shall be a function', () => {
 		expect(typeof valid).toEqual('function');
 	});
 
+	it('shall return true if no model passed', () => {
+		const resp = valid({name: ''}, {});
+		expect(resp).toEqual(true);
+	});
+
 	it('shall not validate a inexistent model', () => {
 		const resp = valid({name: ''}, {model: 'MyTestModel'});
 		expect(resp).toEqual(false);
 	});
-	
 });
